d2d68f45b43c03300c3096f75703ffc7f02dca9f
Devon <devon@example.com>
2026-01-04 16:29:27 +0100

    Implement Flyway migration and TestContainer for testbd

--- apps/api/src/test/java/com/example/api/ApiApplicationTests.java
+++ apps/api/src/test/java/com/example/api/ApiApplicationTests.java
@@ -2,9 +2,30 @@ package com.example.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.PostgreSQLContainer;
+
 
 @SpringBootTest
+@Testcontainers
 class ApiApplicationTests {
+
+	@SuppressWarnings("resource")
+    @Container
+	public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+		.withDatabaseName("testdb")
+		.withUsername("test")
+		.withPassword("test");
+
+	@DynamicPropertySource
+	static void registerDatasourceProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
