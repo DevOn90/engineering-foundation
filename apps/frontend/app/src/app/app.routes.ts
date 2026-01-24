c02c59053cf44c78c885c0ba026849ebdab99c71
Devon <devon@example.com>
2026-01-24 08:18:17 +0100

    Create pages routing

--- apps/frontend/app/src/app/app.routes.ts
+++ apps/frontend/app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HelloPage } from './features/hello/pages/hello-page/hello-page';
+import { HealthPage } from './features/health/pages/health-page/health-page';
+import { UserListPage } from './features/users/pages/user-list-page/user-list-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'hello', pathMatch: 'full'},
+    {path: 'hello', component: HelloPage},
+    {path: 'health', component: HealthPage},
+    {path: 'users', component: UserListPage}
+];
