80b49bc54afa8bfa9bc8f28d845ba3cf30fe5251
Devon <devon@example.com>
2026-01-24 15:43:43 +0100

    file names change and small fixes

--- apps/frontend/app/src/app/core/services/api.ts
+++ apps/frontend/app/src/app/core/services/api.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +11,6 @@ export class ApiService {
   private http = inject(HttpClient);
 
   get<T>(path: string){
-    return this.http.get<T>(path);
+    return this.http.get<T>(`${environment.apiBaseUrl}${path}`);
   }
 }
--- apps/frontend/app/src/environments/environment.dev.ts
+++
@@ -1,5 +0,0 @@
-/* Development Environment Configration */
-
-export const environment = {
-    apiBaseUrl: 'http://localhost:8083'
-};
